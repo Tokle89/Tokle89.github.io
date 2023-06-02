de750904f5a35d8ceac59eef62ccbb2cafa15da9
Tokle89 <fredriktokle@gmail.com>
2023-06-02 18:40:30 +0200

    finished site
    finished styling, and made some typewriter animation in js for the intro section

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 const hamburgerIcon = document.querySelector(".hamburger");
 const nav = document.querySelector("nav");
+const navLinks = document.querySelectorAll("nav a");
 
 hamburgerIcon.addEventListener("click", () => {
   if (nav.classList.contains("show-menu")) {
@@ -8,3 +9,46 @@ hamburgerIcon.addEventListener("click", () => {
     nav.classList.add("show-menu");
   }
 });
+
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    nav.classList.remove("show-menu");
+  });
+});
+
+const introH1 = document.querySelector("h1");
+const introP = document.querySelector("#intro-p");
+let i = 0;
+let x = 0;
+
+const startAnimation = () => {
+  animationH1();
+  setTimeout(animationP, 1500);
+  setTimeout(() => {
+    const cta = document.querySelector("#intro-cta");
+    cta.style.transition = "5s";
+    cta.style.opacity = "100%";
+  }, 5350);
+};
+
+const animationH1 = () => {
+  const text = `My portfolio`;
+
+  if (i < text.length) {
+    introH1.innerHTML += text.charAt(i);
+    i++;
+    setTimeout(animationH1, 70);
+  }
+};
+
+const animationP = () => {
+  const text = `Hello! My Name is Fredrik Tokle. 
+               I am a Frontend Development Student`;
+  if (x < text.length) {
+    introP.innerHTML += text.charAt(x);
+    x++;
+    setTimeout(animationP, 40);
+  }
+};
+
+setTimeout(startAnimation, 1350);
